2f8136f2927728653b316a96fbda91f3fc4d28e6
Nacho Menendez <nchmnndzlpz@gmail.com>
2025-04-21 18:32:39 +0200

    Agregar formularios de autenticación para inicio de sesión y registro, incluyendo validaciones con Zod y componentes de interfaz de usuario.

--- app/(auths)/sign-in/page.tsx
+++ app/(auths)/sign-in/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
+
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
-export default SignIn
+  return <AuthForm schema={SignInSchema} defaultValues={{ email: "", password: "" }} formType="SIGN_IN" onSubmit={(data) => Promise.resolve({ success: true, data })} />;
+};
+export default SignIn;
--- app/(auths)/sign-up/page.tsx
+++ app/(auths)/sign-up/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
+
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
-export default SignUp
+  return <AuthForm schema={SignUpSchema} defaultValues={{ username: "", name: "", email: "", password: "" }} formType="SIGN_UP" onSubmit={(data) => Promise.resolve({ success: true, data })} />;
+};
+export default SignUp;
